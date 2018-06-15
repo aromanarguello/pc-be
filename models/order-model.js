c72e9089dcbbd2cb2235b42bd3b2298fb984982a
Alejandro Roman <aroman@carecloud.com>
2018-06-14 22:23:34 -0400

    added order model with schema

---
+++ models/order-model.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const orderObj = new Schema({
+    name: String,
+    price: Number,
+ }, { _id : false })
+const orderSchema = new Schema (
+    {
+        physicianName: String,
+        patientName: String,
+        order: {type: [orderObj]}
+    }
+)
+
+const OrderModel = mongoose.model('Orders', orderSchema);
+
+module.exports = OrderModel;
